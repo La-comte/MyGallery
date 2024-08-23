d4d430968467663441a15277a57f2ee8fa2168ca
Alexandra Sergeichik <8aleka.serg@gmail.com>
2024-08-23 22:28:25 +0300

    work to sql

---
+++ src/main/java/com/example/buysell/models/Product.java
@@ -0,0 +1,22 @@
+package com.example.buysell.models;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@Entity
+@NoArgsConstructor
+@Table(name = "products")
+public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String title;
+    @Column(columnDefinition = "text")
+    private String description;
+    private int price;
+    private String city;
+    private String author;
+}
---
+++ src/main/java/com/example/buysell/services/ProductService.java
@@ -0,0 +1,29 @@
+package com.example.buysell.services;
+
+import com.example.buysell.models.Product;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    public List<Product> listProducts() { return products; }
+
+    public void saveProduct(Product product) {
+        product.setId(++ID);
+        products.add(product);
+    }
+
+    public void deleteProduct(Long id) {
+        products.removeIf(product -> product.getId().equals(id));
+    }
+
+    public Product getProductById(Long id) {
+        for (Product product : products) {
+            if (product.getId().equals(id)) return product;
+        }
+        return null;
+    }
+}
